8d3010fb3943bbbfda335bfe81361590bccdfbb4
SynxEU <126554945+SynxEU@users.noreply.github.com>
2023-12-19 11:24:05 +0100

    Add project files.

---
+++ Anonymous Methods/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Anonymous_Methods
+{
+    delegate int MyDelegate(int p, int q);
+
+    class Program
+    {
+        static void Main()
+        {
+            MyDelegate arithMethod = (c, d) => c > d ? c : d;
+
+            Console.WriteLine($"Det største af tallene er: {arithMethod(9, 7)}");
+        }
+
+        //static int Max(int c, int d)
+        //{
+        //    if (c > d)
+        //        return c;
+        //    else
+        //        return d;
+        //}
+    }
+}
